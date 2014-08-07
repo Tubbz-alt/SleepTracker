9716d01be895553d5e7c26ce9d4336bcebeec5db
Remy Goldschmidt <taktoa@gmail.com>
2014-08-07 16:04:02 -0400

    Progress bar set up, extraneous fragment removed

--- app/src/main/java/net/fuspr/remy/sleeptracker/SleepButton.java
+++ app/src/main/java/net/fuspr/remy/sleeptracker/SleepButton.java
@@ -1,28 +1,40 @@
 package net.fuspr.remy.sleeptracker;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class SleepButton extends Activity {
+    private TextView log;
+    private SeekBar seek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_button);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
 
+        log = (TextView) findViewById(R.id.logView);
+        seek = (SeekBar) findViewById(R.id.seekBar);
+
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            private int p = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                p = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                log.append(String.format("Progress moved! %d\n", p));
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,26 +48,6 @@ public class SleepButton extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_sleep_button, container, false);
-            return rootView;
-        }
+        return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
